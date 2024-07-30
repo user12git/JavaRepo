a3ce9acebd9ae00845a2491b38710ae1c5b11b23
anjali <anjalidhanawade052@gmail.com>
2024-07-30 22:55:03 +0530

    updated code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // Print "Hello, World!" to the console
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Anjali gabbu dhabbuu!");
     }
 }
 
